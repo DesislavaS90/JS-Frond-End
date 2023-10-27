b3277e79ce44c3f7ed70f471284d108b1d21a649
Desislava Slancheva <137724038+DesislavaS90@users.noreply.github.com>
2023-10-28 00:38:29 +0200

    correction

--- JS Syntax Fundamentals/Lab/7. theatrePromotions.js
+++ JS Syntax Fundamentals/Lab/7. theatrePromotions.js
@@ -1,39 +1,39 @@
 function theatre(day, age) {
     if (age < 0 || age > 122) {
         console.log("Error!");
-        return;
-    }
-
-    let prices = {
-        "Weekday": {
-            "child": 12,
-            "adult": 18,
-            "senior": 12
-        },
-        "Weekend": {
-            "child": 15,
-            "adult": 20,
-            "senior": 15
-        },
-        "Holiday": {
-            "child": 5,
-            "adult": 12,
-            "senior": 10
-        }
-    };
-
-    let ageCategory = "";
-    if (age <= 18) {
-        ageCategory = "child";
-    } else if (age <= 64) {
-        ageCategory = "adult";
     } else {
-        ageCategory = "senior";
-    }
+        let prices = {
+            "Weekday": {
+                "child": 12,
+                "adult": 18,
+                "senior": 12
+            },
+            "Weekend": {
+                "child": 15,
+                "adult": 20,
+                "senior": 15
+            },
+            "Holiday": {
+                "child": 5,
+                "adult": 12,
+                "senior": 10
+            }
+        };
 
-    const result = (prices[day] && prices[day][ageCategory]);
-    console.log(`${result}$`);
+        let ageCategory = "";
+        if (age <= 18) {
+            ageCategory = "child";
+        } else if (age <= 64) {
+            ageCategory = "adult";
+        } else {
+            ageCategory = "senior";
+        }
+
+        const result = (prices[day] && prices[day][ageCategory]);
+        console.log(`${result}$`);
+    }
 }
 
 
-theatre('Holiday', 15)
+
+theatre('Holiday', -5)
