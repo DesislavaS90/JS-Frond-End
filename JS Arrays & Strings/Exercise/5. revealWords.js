687ae1932c2e5edb93721882e28c4851b2a32c5a
Desislava Slancheva <137724038+DesislavaS90@users.noreply.github.com>
2023-11-07 23:57:03 +0100

    corrections needed

--- JS Arrays & Strings/Exercise/5. revealWords.js
+++ JS Arrays & Strings/Exercise/5. revealWords.js
@@ -1,14 +1,22 @@
 function revealWords(words, text){
-    let wordsArr = words.split(", ")
-    let textArr = text.split(", ")
+    let wordsArr = words.split(" ")
+    let textArr = text.split(" ")
+    
 
     for (let i = 0; i < wordsArr.length; i++){
          
-        for (let t = 0; i < textArr.length; i++){
+        for (let t = 0; t < textArr.length; t++){
 
-            if (wordsArr[i].length === textArr.length)
+            if (wordsArr[i].length === textArr[t].length && textArr[t].startsWith("*")){
+                textArr[t].replace(wordsArr[i])
+
+            }
         }
     }
+
+    console.log(textArr.join(" "))
+
+    
 }
 
 revealWords('great','softuni is ***** place for learning new programming languages')
