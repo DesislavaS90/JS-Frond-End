824d8cea62373869dd32bb0fd5b5b46e6e4b8253
Desislava Slancheva <137724038+DesislavaS90@users.noreply.github.com>
2023-12-03 01:44:12 +0100

    correction 2

--- DOM Intro/Exercise/02. Pascal or Camel Case/solution.js
+++ DOM Intro/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,35 +1,35 @@
-function camelCase(text){
-  let result = [];
-
-  let textArr = text.split(' ').map(x => x.toLowerCase());;
-  result.push(textArr[0]);
-
-  
-  for (let i = 1; i < textArr.length; i++) {
-     result.push(textArr[i].charAt(0).toUpperCase() + textArr[i].slice(1));
-  }
-
-  return result.join('');
-
-}
-
-function pascalCase(text) {
-  let result = [];
-
-  let textArr = text.split(' ').map(x => x.toLowerCase());
-  
-  for (let index = 0; index < textArr.length; index++) {
-    result.push(textArr[index].charAt(0).toUpperCase() + textArr[index].slice(1));
-  }
-
-  return result.join('');
-  
-}
-
 function solve() {
   let text = document.getElementById('text').value;
   let convention = document.getElementById('naming-convention').value;
   let result = '';
+
+  function camelCase(text){
+    let result = [];
+  
+    let textArr = text.split(' ').map(x => x.toLowerCase());;
+    result.push(textArr[0]);
+  
+    
+    for (let i = 1; i < textArr.length; i++) {
+       result.push(textArr[i].charAt(0).toUpperCase() + textArr[i].slice(1));
+    }
+  
+    return result.join('');
+  
+  }
+  
+  function pascalCase(text) {
+    let result = [];
+  
+    let textArr = text.split(' ').map(x => x.toLowerCase());
+    
+    for (let index = 0; index < textArr.length; index++) {
+      result.push(textArr[index].charAt(0).toUpperCase() + textArr[index].slice(1));
+    }
+  
+    return result.join('');
+    
+  }
 
   if (convention === 'Camel Case') {
     result = camelCase(text);
--- DOM Intro/Exercise/07. Hell's Kitchen/app.js
+++ DOM Intro/Exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,13 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
+   let input = document.querySelector('textarea').value;
+   let bestRestaurant = document.querySelector('#bestRestaurant > p').textContent;
+   let workers = document.querySelector('#workers > p').textContent;
+
+
    function onClick () {
-      //   TODO:
-      
-   }
+      console.log(JSON.parse(input));
+
+   }     
 }
