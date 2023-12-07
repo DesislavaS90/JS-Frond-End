2c5477dfdef52ebf4ba40c2c38e355670c8e3187
Desislava Slancheva <137724038+DesislavaS90@users.noreply.github.com>
2023-12-07 17:10:44 +0100

    table report

--- DOM Intro/Exercise/08. Generate Report/report.js
+++ DOM Intro/Exercise/08. Generate Report/report.js
@@ -1,27 +1,31 @@
 function generateReport() {
+    const checkBoxes = document.querySelectorAll('thead tr th input');
+    const table = Array.from(document.querySelectorAll('tbody tr'));
+    const output = document.getElementById('output');
 
-    let checkBoxes = document.querySelectorAll('thead tr th input');
-    let boxName = document.querySelectorAll('thead tr')[0].textContent
-    let tableInfo = document.querySelectorAll('tbody tr');
-    
-    let result  = []
-    let personObj = {}
-    
+    const colInfo = {};
+    const tableInfoChecked = [];
+
+   
     for (let i = 0; i < checkBoxes.length; i++) {
         if (checkBoxes[i].checked) {
-            personObj[boxName[i]] = tableInfo[i].textContent
+            colInfo[checkBoxes[i].name] = i;
         }
     }
 
-
-
-
-    
-
-}
-
    
+    for (let i = 0; i < table.length; i++) {
+        const rowInfo = {};
+
+        
+        for (const [key, value] of Object.entries(colInfo)) {
+            rowInfo[key] = table[i].children[value].textContent;
+        }
+
+        
+        tableInfoChecked.push(rowInfo);
+    }
 
     
-
-
+    output.textContent = JSON.stringify(tableInfoChecked, null, 2);
+}
